d12b2c79af095c47189d27497126821cdf14bfad
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-08-20 14:59:29 -0300

    testing users-controllers REGISTER

--- tests/controller/users-controllers.test.ts
+++ tests/controller/users-controllers.test.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
-import { loginUser } from "../../src/controller/users-controllers";
-import { loginUserService, generateToken } from "../../src/service/users-services";
+import { loginUser, registerUser } from "../../src/controller/users-controllers";
+import { loginUserService, registerUserService, generateToken } from "../../src/service/users-services";
 
 jest.mock('../../src/service/users-services');
 
 const mockedloginUserService = loginUserService as jest.Mock;
+const mockedRegisterUserService = registerUserService as jest.Mock;
 const mockedGenerateToken = generateToken as jest.Mock;
 
 describe('loginUser CONTROLLER', () => {
@@ -61,5 +62,66 @@ describe('loginUser CONTROLLER', () => {
       message: 'an error occurred while trying to login',
       OK: false, 
     });
+  });
+});
+
+describe('registerUser CONTROLLER', () => {
+  it('should return a token and message on successful register user', async () => {
+    const req = {
+      body: {
+        userName: 'testing123',
+        userEmail: 'testing@examble.com',
+        userPassword: 'password123',
+        userAge: 20,
+        userCity: 'Maringá',      
+      }
+    } as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      send: jest.fn(),
+    } as unknown as Response;
+
+    const user = { _id: '123', userName: 'testing123', userEmail: 'testing@examble.com', userPassword: 'password123', userAge: 20, userCity: 'Maringá' };
+    mockedRegisterUserService.mockResolvedValue({
+      user, 
+      message: 'CADASTRO OK',
+      OK: true,
+    });
+    mockedGenerateToken.mockReturnValue('token123')
+
+    await registerUser(req, res);
+
+    expect(mockedRegisterUserService).toHaveBeenCalledWith({ userName: 'testing123', userEmail: 'testing@examble.com', userPassword: 'password123', userAge: 20, userCity: 'Maringá' });
+    expect(mockedGenerateToken).toHaveBeenCalledWith('123');
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith({ userToken: 'token123', message: 'CADASTRO OK',  OK: true })
+  });
+
+  it('should return a 500 status if register user fails', async () => {
+    const req = {
+        body: {
+          userName: 'testing123',
+          userEmail: 'testing@examble.com',
+          userPassword: 'password123',
+          userAge: 20,
+          userCity: 'Maringá',      
+        }
+      } as Request;
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      } as unknown as Response;
+
+      mockedRegisterUserService.mockResolvedValue(undefined);
+
+      await registerUser(req, res);
+
+      expect(mockedRegisterUserService).toHaveBeenCalledWith(req.body);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ message: 'an error occurred while trying to register', OK: false })
   });
 });
